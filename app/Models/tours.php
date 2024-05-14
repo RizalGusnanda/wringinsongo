5c3286a8e0c595baf393c1becdde2bf29d18b475
RizalGusnanda <muchamadrizalgusnanda@gmail.com>
2024-05-14 23:51:44 +0700

    fix model

--- app/Models/tours.php
+++ app/Models/tours.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class tours extends Model
+class Tours extends Model
 {
     use HasFactory;
     protected $fillable = [
