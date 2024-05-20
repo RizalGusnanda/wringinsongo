201c9941a24fc7f2f698f08b5cfb58e396663383
RizalGusnanda <muchamadrizalgusnanda@gmail.com>
2024-05-20 23:21:55 +0700

    fix carts

--- database/seeders/CartsSeeder.php
+++ database/seeders/CartsSeeder.php
@@ -15,14 +15,14 @@ class CartsSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('carts')->insert([
-            'id_ticket' => 1,
-            'id_tour' => 1,
-            'total_price' => '25000',
-            'status' => 'pending',
-            'status_confirm' => 'pending',
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
+        // DB::table('carts')->insert([
+        //     'id_ticket' => 1,
+        //     'id_tour' => 1,
+        //     'total_price' => '25000',
+        //     'status' => 'pending',
+        //     'status_confirm' => 'pending',
+        //     'created_at' => now(),
+        //     'updated_at' => now(),
+        // ]);
     }
 }
