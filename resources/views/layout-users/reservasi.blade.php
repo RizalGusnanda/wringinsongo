95e85d8e80825cac243cd9bb14741c230b1d45b0
RizalGusnanda <muchamadrizalgusnanda@gmail.com>
2024-06-06 22:25:08 +0700

    button detail

--- resources/views/layout-users/reservasi.blade.php
+++ resources/views/layout-users/reservasi.blade.php
@@ -13,29 +13,35 @@
                             <div class="row">
                                 <div class="col-md-4">
                                     <div class="profile-widget-description mx-4 mt-4">
-                                        <h5 class="card-title font-weight-bold d-block mx-2 profile-title-card1 text-center">
+                                        <h5
+                                            class="card-title font-weight-bold d-block mx-2 profile-title-card1 text-center">
                                             Sortir
                                         </h5>
                                         <div class="sortir">
-                                            <a href="/reservasi-user" class="btn btn-primary btn-block mb-2 btn-custom-str">Semua</a>
-                                            <a href="/reservasi-user?status=aktif" class="btn btn-primary btn-block mb-2 btn-custom-str">Aktif</a>
-                                            <a href="/reservasi-user?status=selesai" class="btn btn-primary btn-block mb-2 btn-custom-str">Selesai</a>
+                                            <a href="/reservasi-user"
+                                                class="btn btn-primary btn-block mb-2 btn-custom-str">Semua</a>
+                                            <a href="/reservasi-user?status=aktif"
+                                                class="btn btn-primary btn-block mb-2 btn-custom-str">Aktif</a>
+                                            <a href="/reservasi-user?status=selesai"
+                                                class="btn btn-primary btn-block mb-2 btn-custom-str">Selesai</a>
                                         </div>
                                     </div>
                                 </div>
 
                                 <div class="col-md-8">
                                     <div class="mb-4">
-                                        <h5 class="card-title font-weight-bold d-block mt-4 mx-2 mb-3 profile-title-card4 text-center">
+                                        <h5
+                                            class="card-title font-weight-bold d-block mt-4 mx-2 mb-3 profile-title-card4 text-center">
                                             Data Reservasi
                                         </h5>
-                                        
+
                                         @if ($reservasi->isEmpty())
                                             <div class="alert alert-warning text-center mx-4" role="alert">
                                                 @if (request()->query('status', 'all') == 'all')
                                                     Belum terdapat data reservasi.
                                                 @else
-                                                    Tidak ada reservasi ditemukan untuk filter "{{ request()->query('status') }}".
+                                                    Tidak ada reservasi ditemukan untuk filter
+                                                    "{{ request()->query('status') }}".
                                                 @endif
                                             </div>
                                         @else
@@ -44,7 +50,8 @@
                                                     <div class="card-body dt-res">
                                                         <div class="row align-items-center">
                                                             <div class="col-md-4 d-flex justify-content-center">
-                                                                <img src="{{ Storage::url($cart->tour->profile_tour) }}" class="mage-reser" alt="Foto Tiket">
+                                                                <img src="{{ Storage::url($cart->tour->profile_tour) }}"
+                                                                    class="mage-reser" alt="Foto Tiket">
                                                             </div>
                                                             <div class="col-md-8 rs-ws">
                                                                 <h5 class="card-title-res">{{ $cart->tour->name }}</h5>
@@ -65,9 +72,15 @@
                                                                     </div>
                                                                     <div class="col-4 text-right btn-test">
                                                                         @if ($cart->status_confirm === 'success' && !$cart->hasTestimonial)
-                                                                            <a href="/testimoni?id_tour={{ $cart->tour->id }}&id_cart={{ $cart->id }}" class="btn btn-testimoni">Testimoni</a>
+                                                                            <a href="/testimoni?id_tour={{ $cart->tour->id }}&id_cart={{ $cart->id }}"
+                                                                                class="btn btn-testimoni">Testimoni</a>
                                                                         @elseif ($cart->status_confirm === 'success' && $cart->hasTestimonial)
-                                                                            <button class="btn btn-success px-3" style="border-radius: 20px;">Selesai</button>
+                                                                            <button class="btn btn-success px-3"
+                                                                                style="border-radius: 20px;">Selesai</button>
+                                                                        @else
+                                                                            <button class="btn btn-warning px-3 text-white"
+                                                                                style="border-radius: 20px;">Lihat
+                                                                                Detail</button>
                                                                         @endif
                                                                     </div>
                                                                 </div>
